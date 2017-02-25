f1c1d15351773729d155e9cc269da99cac6a42f1
Roger Watkins <roger.watkins@gnomesoft.co.uk>
2017-02-25 20:40:45 +0000

    load swagger file from uri

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ var app = require('connect')();
 var http = require('http');
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
-var fs = require('fs');
+var request = require('request');
 
 var serverPort = normalizePort(process.env.PORT || '8080');
 
 // swaggerRouter configuration
 var options = {
-  swaggerUi: '/swagger.json',
-  controllers: './lib/controllers',
-  useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
+    swaggerUi: '/swagger.json',
+    controllers: './lib/controllers',
+    useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
 };
 
 
@@ -37,26 +37,41 @@ function normalizePort(val) {
 }
 
 // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync('./lib//api/swagger.yaml', 'utf8');
-var swaggerDoc = jsyaml.safeLoad(spec);
+var swaggerUri = 'https://s3-eu-west-1.amazonaws.com/hermesapi/Schemas/Locations/v1/swagger.yaml';
+request(swaggerUri,
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            initServer(body);
+        } else {
+            console.log("Unable to read swagger file from " + swaggerUri + " - server will not start.");
+        }
+    });
+
 
 // Initialize the Swagger middleware
-swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
-  // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
-  app.use(middleware.swaggerMetadata());
 
-  // Validate Swagger requests
-  app.use(middleware.swaggerValidator());
+function initServer(spec) {
 
-  // Route validated requests to appropriate controller
-  app.use(middleware.swaggerRouter(options));
+    var swaggerDoc = jsyaml.safeLoad(spec);
 
-  // Serve the Swagger documents and Swagger UI
-  app.use(middleware.swaggerUi());
+    swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+            // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+            app.use(middleware.swaggerMetadata());
 
-  // Start the server
-  http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
-  });
-});
+            // Validate Swagger requests
+            app.use(middleware.swaggerValidator());
+
+            // Route validated requests to appropriate controller
+            app.use(middleware.swaggerRouter(options));
+
+            // Serve the Swagger documents and Swagger UI
+            app.use(middleware.swaggerUi());
+
+            // Start the server
+            http.createServer(app).listen(serverPort, function () {
+                console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+                console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+            });
+        }
+    );
+}
