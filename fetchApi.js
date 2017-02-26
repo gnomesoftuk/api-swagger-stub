9e96f701b481e949c3aaac8ca9fd5a15323ebc8c
Roger Watkins <roger.watkins@gnomesoft.co.uk>
2017-02-26 00:32:34 +0000

    move swagger uri into config file (api.json)

--- fetchApi.js
+++ fetchApi.js
@@ -4,7 +4,9 @@ module.exports = function generateApi (done) {
     var request = require('request'),
         fs = require('fs');
 
-    var swaggerUri = 'https://s3-eu-west-1.amazonaws.com/hermesapi/Schemas/Locations/v1/swagger.yaml';
+    var apiConfig = JSON.parse(fs.readFileSync("api.json", "utf-8"));
+    var swaggerUri = apiConfig.swaggerUri;
+
     request(swaggerUri,
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
